f1185e38840341e0c483b1041452127df3bd0754
ghenoo <lucasgheno600@gmail.com>
2024-07-10 19:14:39 -0300

    commit: att usecase and product

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -1,4 +1,14 @@
 package usecase
 
-type productUsecase struct {
+import "go-api/model"
+
+type ProductUsecase struct {
+}
+
+func NewProductUseCase() ProductUsecase {
+	return ProductUsecase{}
+}
+
+func (pu *ProductUsecase) GetProduct() ([]model.Product, error) {
+	return []model.Product{}, nil
 }
